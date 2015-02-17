f3d7ab76ee6ad2ecbf5242400a1550f880e2c551
Sam Frances <sam@samfrances.co.uk>
2015-02-17 13:31:58 +0000

    Change main.py to use view.py instead of basic_view.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import media, basic_view, server
+import media, view, server
 
 pans_labyrinth = media.Movie(imdb_id="tt0457430", 
                              trailer_youtube_url="https://www.youtube.com/watch?feature=player_detailpage&v=EqYiSlkvRuw#t=6")
@@ -21,6 +21,6 @@ four_lions = media.Movie(imdb_id="tt1341167",
 movies = [pans_labyrinth, monty_python, todo_sobre_mi_madre, volver, four_lions, twenty_eight]
 
 if __name__ == "__main__":
-    html = basic_view.movies_view(*movies)
+    html = view.movies_view(*movies)
     srv = server.VerySimpleServer(html)
     srv.run()
