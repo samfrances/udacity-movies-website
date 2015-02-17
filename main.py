18ccc014239c120667e13c82223f18fa9c5ac493
Sam Frances <sam@samfrances.co.uk>
2015-02-17 01:16:54 +0000

    Add main.py, and create 6 Movie objects

---
+++ main.py
@@ -0,0 +1,21 @@
+import media, fresh_tomatoes
+
+pans_labyrinth = media.Movie(imdb_id="tt0457430", 
+                             trailer_youtube_url="https://www.youtube.com/watch?feature=player_detailpage&v=EqYiSlkvRuw#t=6")
+
+monty_python = media.Movie(imdb_id="tt0071853",
+                           trailer_youtube_url="https://www.youtube.com/watch?feature=player_detailpage&v=fxGqcCeV3qk#t=142")
+
+todo_sobre_mi_madre = media.Movie(imdb_id="tt0185125", 
+                                  trailer_youtube_url="https://www.youtube.com/watch?v=-PFVGvsTKXo")
+
+twenty_eight = media.Movie(imdb_id="tt0289043",
+                           trailer_youtube_url="https://www.youtube.com/watch?v=02qibJJ0OvE")
+
+volver = media.Movie(imdb_id="tt0441909",
+                     trailer_youtube_url="https://www.youtube.com/watch?feature=player_detailpage&v=9L2AJmNoUgo#t=4")
+
+four_lions = media.Movie(imdb_id="tt1341167",
+                         trailer_youtube_url="https://www.youtube.com/watch?v=nxJlqapu3zE")  
+
+movies = [pans_labyrinth, monty_python, todo_sobre_mi_madre, volver, four_lions, twenty_eight]
