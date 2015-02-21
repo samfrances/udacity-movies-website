ad304959bddaeabcb4ac250b9c94be3d32c74edd
Sam Frances <sam@samfrances.co.uk>
2015-02-21 12:45:55 +0000

    Change to 'from' imports in main.py

--- main.py
+++ main.py
@@ -2,38 +2,41 @@
 objects, passes them to the rendering function, and starts the server to
 serve the resulting html."""
 
-import media, view, server
+from media import Movie
+from view import movies_view
+from server import VerySimpleServer
 
-pans_labyrinth = media.Movie(imdb_id="tt0457430", 
-                             trailer_youtube_id="EqYiSlkvRuw")
 
-monty_python = media.Movie(imdb_id="tt0071853",
-                           trailer_youtube_id="fxGqcCeV3qk")
+pans_labyrinth = Movie(imdb_id="tt0457430", 
+                       trailer_youtube_id="EqYiSlkvRuw")
 
-todo_sobre_mi_madre = media.Movie(imdb_id="tt0185125", 
-                                  trailer_youtube_id="-PFVGvsTKXo")
+monty_python = Movie(imdb_id="tt0071853",
+                     trailer_youtube_id="fxGqcCeV3qk")
 
-twenty_eight = media.Movie(imdb_id="tt0289043",
-                           trailer_youtube_id="02qibJJ0OvE")
+todo_sobre_mi_madre = Movie(imdb_id="tt0185125", 
+                            trailer_youtube_id="-PFVGvsTKXo")
 
-volver = media.Movie(imdb_id="tt0441909",
-                     trailer_youtube_id="9L2AJmNoUgo")
+twenty_eight = Movie(imdb_id="tt0289043",
+                     trailer_youtube_id="02qibJJ0OvE")
 
-four_lions = media.Movie(imdb_id="tt1341167",
-                         trailer_youtube_id="Ew-SrlQ9tlI")  
+volver = Movie(imdb_id="tt0441909",
+               trailer_youtube_id="9L2AJmNoUgo")
 
-goodbye_lenin = media.Movie(imdb_id="tt0301357",
-                trailer_youtube_id="u5hzmwGW4Ac")
+four_lions = Movie(imdb_id="tt1341167",
+                   trailer_youtube_id="Ew-SrlQ9tlI")  
 
-two_days = media.Movie(imdb_id="tt2737050",
-                       trailer_youtube_id="InuzW58ydyU")
+goodbye_lenin = Movie(imdb_id="tt0301357",
+                      trailer_youtube_id="u5hzmwGW4Ac")
+
+two_days = Movie(imdb_id="tt2737050",
+                 trailer_youtube_id="InuzW58ydyU")
                        
-spirited = media.Movie(imdb_id="tt0245429",
-                       trailer_youtube_id="ByXuk9QqQkk")
+spirited = Movie(imdb_id="tt0245429",
+                 trailer_youtube_id="ByXuk9QqQkk")
 
 movies = [pans_labyrinth, monty_python, todo_sobre_mi_madre, volver, four_lions, twenty_eight, goodbye_lenin, two_days, spirited]
 
 if __name__ == "__main__":
-    html = view.movies_view(*movies)
-    srv = server.VerySimpleServer(html)
+    html = movies_view(*movies)
+    srv = VerySimpleServer(html)
     srv.run()
